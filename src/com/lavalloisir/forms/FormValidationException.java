968a03a7aa9c1097d780453d966394f47fa55554
Pierre Marchal <p.marchal@iia-laval.fr>
2015-04-15 15:03:03 +0200

    Adding serialization constant

--- src/com/lavalloisir/forms/FormValidationException.java
+++ src/com/lavalloisir/forms/FormValidationException.java
@@ -1,6 +1,11 @@
 package com.lavalloisir.forms;
 
 public class FormValidationException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5370666563471455407L;
+
 	// CONSTRUCTOR
 	public FormValidationException(String message) {
 		super(message);
